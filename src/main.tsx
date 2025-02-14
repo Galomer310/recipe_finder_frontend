292ba998a775774528c64508947f9f8df3e1d512
Gal Omer <galomer6708@gmail.com>
2025-02-14 14:46:32 +0200

    style css

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import "./style.css";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
